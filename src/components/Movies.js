957754ecaee85a2eb65877ea5dd56dbe31fb5823
I-Am-Lain <vetter.william@gmail.com>
2020-11-28 21:21:38 -0600

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie=> {
+          return <div>
+            <h2>{movie.title}</h2>
+            <h3>{movie.time}</h3>
+            <ul>
+              {
+                movie.genres.map(genre => {
+                  return <ul>{genre}</ul>
+                })
+              }
+            </ul>
+          </div>
+        })}
     </div>
   );
 };
